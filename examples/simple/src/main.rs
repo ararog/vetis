14bf8019bfd9036331fd2202951b9ce7d06c7023
Rogério Araújo <rogerio.araujo@gmail.com>
2026-01-21 13:03:48 -0300

    docs: added simple example

---
+++ examples/simple/src/main.rs
@@ -0,0 +1,30 @@
+use vetis::{Vetis, server::config::ServerConfig};
+use http_body_util::{Full};
+use bytes::Bytes;
+use hyper::Response;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let port = 8080;
+    let interface = "::".to_string();
+
+    let config = ServerConfig::builder()
+        .port(port)
+        .interface(interface)
+        .build();
+
+
+    let mut server = Vetis::new(config);
+
+    server.start(|_| async move {
+        Ok(Response::new(Full::new(Bytes::from("Hello World"))))
+    }).await?;
+
+    println!("Server listening on port {}:{}", server.config().interface(), server.config().port());
+
+    loop {
+      
+    }
+
+    Ok(())
+}
